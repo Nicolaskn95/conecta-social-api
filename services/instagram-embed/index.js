bee6851d1e03c6025cfb66c870bdfd21723c1238
Maicon Santos <maicon.santos71@outlook.com>
2025-10-29 14:20:31 -0300

    fix(instagram): atualizar estilo do bloco de embed do Instagram

--- services/instagram-embed/index.js
+++ services/instagram-embed/index.js
@@ -178,7 +178,7 @@ function generateInstagramEmbed(postId, originalUrl) {
   return `<blockquote class="instagram-media"
   data-instgrm-permalink="${originalUrl}"
   data-instgrm-version="14"
-  style="max-width:540px; min-width:326px; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
+  style="border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:540px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
 </blockquote>`;
 }
 
